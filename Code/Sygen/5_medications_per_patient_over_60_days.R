b0849d43b899160b191386edf2ce8cf714ca274e
jutzca <Catherine.Jutzeler@bsse.ethz.ch>
2021-03-10 16:08:54 +0100

    update polypharmacy cu code

--- Code/Sygen/5_medications_per_patient_over_60_days.R
+++ Code/Sygen/5_medications_per_patient_over_60_days.R
@@ -75,7 +75,7 @@ sygen.original.subset1 <- distinct(subset(sygen.original.subset, Time==0 | Time=
 
 # Merge medication file and demographics file
 joined_mediction.df <- merge(medication.per.patient, sygen.original.subset1, by.x = "NEW_ID", 
-                   by.y = "ID", all.x = TRUE, all.y = FALSE)
+                   by.y = "ID", all.x = FALSE, all.y = TRUE)
 
 #---------- Assign new ID for the patients (for data protection reason) ---------- 
 joined_mediction.df.new.id<-joined_mediction.df %>% 
@@ -91,11 +91,21 @@ id <- joined_mediction.df.new.id[order(joined_mediction.df.new.id$ID),]
 id_split <- split(id, id$ID)
 
 new_names <- as.character(unique(joined_mediction.df.new.id$ID))
+# plegia <- as.character(unique(joined_mediction.df.new.id$NLI))
+# nli <- as.character(unique(joined_mediction.df.new.id$NLI_raw))
 
 for (i in 1:length(id_split)) {
   id_split[[i]]
-  write.csv(id_split[[i]], paste("/Volumes/jutzelec$/8_Projects/1_Ongoing/3_Drugs/pid_graphs/",new_names[i],".csv", sep=""))
+  ais.grades <- unique(id_split[[i]]$AIS)
+  sex <- unique(id_split[[i]]$Sex)
+  nli <- unique(id_split[[i]]$NLI_raw)
+  plegia <- unique(id_split[[i]]$NLI)
+  write.csv(id_split[[i]], paste("/Volumes/jutzelec$/8_Projects/1_Ongoing/3_Drugs/pid_graphs/",new_names[i],"_",ais.grades,"_", sex,"_", nli,"_",plegia,".csv", sep=""))
 }
+
+
+
+
 
 
 #---------- Plot drug exposure pattern for each patient ---------- 
@@ -108,7 +118,7 @@ for(file in file_list)    #repeat for all files in dir folder
 {
   data <- read.csv(file, header=TRUE, sep=',')
   
-  data<- read.csv(file.choose())
+  #data<- read.csv(file.choose())
   data1 <- data
 
   cols_to_change = c(5:368)    #change columns 4:368 to numerics class format
@@ -117,31 +127,39 @@ for(file in file_list)    #repeat for all files in dir folder
     class(data1[, i]) = "numeric"
     }
   
+  # Sum up all lines with same drugs per patient
+  datan<-plyr::ddply(data1,.(ID, generic.name, indication,Sex, Age, AIS, Cause, NLI, NLI_raw, YEARDOI,Time_wks, New_timeline), function(x) colSums(x[,-c(1,2,3,4,66:382)], na.rm = TRUE))
 
-  datan<-plyr::ddply(data1,.(ID, generic_name, indication,Sex, Age, AIS, Cause, NLI, NLI_raw, YEARDOI,Time_wks, New_timeline ), function(x) colSums(x[,-c(1,2,3,4,66:382)], na.rm = TRUE))
-  
+# Reformat data from wide to long
   data_long<-datan%>%
     gather(day, daily_dose, X0:X60)
 
   data_long$day<- sub("X","",data_long$day)
   data_long$day<- as.numeric(data_long$day)
-  data_long$daily_dose<- as.numeric(data_long$daily_dose)
+
+  ais.grade.plot <-unique(data_long$AIS)
+  sex.plot <-unique(data_long$Sex)
+  cause.plot <-unique(data_long$Cause)
+  plegia.plot <-unique(data_long$NLI)
+  nli.plot <-unique(data_long$NLI_raw)
   
+  # colors <- colorRampPalette(c("white", "#bca0dc", "#b491c8", "#663a82", "#3c1361"))(8)
   
+  colors <- colorRampPalette(c("white", "#0000ff"))(7)
   
-  
-  myplot1<- ggplot(data_long, aes(day, generic_name, fill=daily_dose))+geom_tile(color = "white") +
-        scale_fill_gradient(low = "white", high="black") +
-    theme_linedraw()+scale_x_continuous(expand = c(0, 0), breaks = c(0,10,20,30,60))+ 
-    # ggtitle(stringr::str_sub(file, end=-5))+
-     #ggtitle(paste(file))+ 
-    labs(x="Days Post-Injury")+ 
-    theme(panel.grid.major = element_blank(),axis.title.x = element_text(size = 12),
-          axis.text.x = element_text(color="black", size=10), 
-          axis.text.y = element_text( color="gray28", size=9), 
-          axis.title.y  = element_blank(), legend.position = "none")
+# Create plot  
+myplot1<- ggplot(data_long, aes(day, generic.name, fill=as.factor(daily_dose)))+geom_tile(color = "white") +
+  scale_fill_manual(values=colors)+theme_linedraw()+scale_x_continuous(expand = c(0, 0), breaks = c(0,15,30,45,60))+ 
+    ggtitle(paste(sex.plot,", ",ais.grade.plot,", ",plegia.plot," (",nli.plot,"), ",cause.plot, sep = ""))+ 
+    labs(x="Days Post-Injury", fill = "Number of\n Doses")+ 
+    theme(panel.grid.major = element_blank(),axis.title.x = element_text(size = 12, family = 'Times'),
+          plot.title =  element_text(size = 14, family = 'Times', face='bold'),
+          axis.text.x = element_text(color="black", size=10, family = 'Times'), 
+          axis.text.y = element_text( color="black", size=10, family = 'Times'), 
+          axis.title.y  = element_blank(), legend.key = element_rect(fill = "black", color = NA))
   myplot1
   
+  # Save Plot
   ggsave(myplot1,filename=paste(stringr::str_sub(file, end=-5),".pdf",sep=""),path='/Users/jutzca/Documents/Github/Acute-Pharmacological-Treatment-in-SCI/Figures/Sygen/Polypharmacy')
   
 }
